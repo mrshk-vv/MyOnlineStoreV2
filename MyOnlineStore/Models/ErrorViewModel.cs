a7c200619640e834b25c8aac39a33016dc0d46e4
Владимир Мурашко <murashko_volodimir@hotmail.com>
2020-09-05 19:41:43 +0300

    Добавьте файлы проекта.

---
+++ MyOnlineStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyOnlineStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
